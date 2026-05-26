3ff3889b18cb9521e308301e0dc281ec2ae7b06a
Artem <artyom.lovczov@mail.ru>
2026-05-26 20:25:11 +0400

    Создал базовый образ заявки и CRUD операции с ним

--- application-service/src/main/java/com/example/applicationservice/application/web/CreateApplicationRequest.java
+++ application-service/src/main/java/com/example/applicationservice/application/web/CreateApplicationRequest.java
@@ -2,7 +2,6 @@ package com.example.applicationservice.application.web;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
