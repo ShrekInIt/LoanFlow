f2ab3f291ee4981e3195ba6dd88b7f77c3d9020e
Artem <artyom.lovczov@mail.ru>
2026-05-31 14:52:30 +0400

    Добавил saga

--- scoring-service/src/main/java/com/example/scoringservice/outbox/OutboxEventPublisherJob.java
+++ scoring-service/src/main/java/com/example/scoringservice/outbox/OutboxEventPublisherJob.java
@@ -3,7 +3,6 @@ package com.example.scoringservice.outbox;
 import com.example.scoringservice.kafka.ScoringEventPublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
